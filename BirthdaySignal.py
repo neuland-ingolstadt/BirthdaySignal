42a7895c2e8dd548dfede3e65c01c2ace2f40826
Jen <j.hofbauer223@t-online.de>
2021-04-15 19:36:32 +0200

    Update BirthdaySignal.py

--- BirthdaySignal.py
+++ BirthdaySignal.py
@@ -2,13 +2,11 @@ from datetime import datetime
 import csv
 import os
 
-currentDate = datetime.today().strftime('%Y-%m-%d')
+currentDate = datetime.today().strftime('%m-%d')
 
-import csv
 with open('BirthdaySignal.csv', 'r') as file:
 	reader = csv.reader(file, delimiter=';')
 	for row in reader:
-		if(row[2] == currentDate):
-			message = 'Alles Gute zum Geburtstag ' +  row[1] + '! 🎉🎂 Wir wünschen dir einen schönen Tag und weiterhin eine positive Erfahrung bei uns im Verein. 😊 Dein Team von Neuland Ingolstadt e.V.'
-			message = "'" + message.replace("'", "'\\''") + "'"
-			os.system('./signal-cli-0.8.1/bin/signal-cli -u +49<TelephonenumberSender> send -m ' + message + ' ' + row[0])
+		if row[2] == currentDate and row[1].isalpha() and row[0][1:].isdecimal() and row[0][0] == '+':
+			message = '\'Alles Gute zum Geburtstag ' +  row[1] + '! 🎉🎂 Wir wünschen dir einen schönen Tag und weiterhin eine positive Erfahrung bei uns im Verein. 😊 Dein Team von Neuland Ingolstadt e.V.\''
+			os.system('signal-cli -u +49<TelephonenumberSender> send -m ' + message + ' ' + row[0])
