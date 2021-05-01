b46562659dfa47a46bc91ff954de428a4e83da63
Alexander Horn <alexhorn@users.noreply.github.com>
2021-05-01 18:55:01 +0200

    Read username from environment variable

--- BirthdaySignal.py
+++ BirthdaySignal.py
@@ -9,4 +9,4 @@ with open('BirthdaySignal.csv', 'r') as file:
 	for row in reader:
 		if row[2] == currentDate and row[1].isalpha() and row[0][1:].isdecimal() and row[0][0] == '+':
 			message = '\'Alles Gute zum Geburtstag ' +  row[1] + '! 🎉🎂 Wir wünschen dir einen schönen Tag und weiterhin eine positive Erfahrung bei uns im Verein. 😊 Dein Team von Neuland Ingolstadt e.V.\''
-			os.system('signal-cli -u +49<TelephonenumberSender> send -m ' + message + ' ' + row[0])
+			os.system('signal-cli -u $USERNAME send -m ' + message + ' ' + row[0])
